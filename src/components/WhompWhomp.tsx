9c4682bea83214bb56e9a4b4478646016c592c0e
ydrExty <liaroffear@mail.ru>
2022-08-16 17:18:08 +0500

    Fixed img border

--- src/components/WhompWhomp.tsx
+++ src/components/WhompWhomp.tsx
@@ -8,10 +8,12 @@ export const WhompWhomp = (props: DatoCmsPost) => {
 
   return (
     <div className="w-4/6 h-64 border-2 rounded-2xl flex flex-row">
-      <GatsbyImage 
-        image={image} alt=""
-        className="h-full w-8/12 border-2xl border-hidden rounded-2xl box-border"
-      />
+      <div className="h-full w-8/12 rounded-2xl box-border">
+        <GatsbyImage 
+          image={image} alt=""
+          className="h-full w-full rounded-2xl box-border"
+        />
+      </div>
       <div className="h-full w-4/12 rounded-2xl box-border">
         {props.title}
       </div>
