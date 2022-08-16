3093b23f80887dc7f59fb26b35d17ac4367b026a
ydrExty <liaroffear@mail.ru>
2022-08-16 19:24:14 +0500

    Fixed queries

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/Layout';
+import { StaticImage } from "gatsby-plugin-image"
 
-export default function NotFound() {
+const NotFound = () => {
   return (
     <Layout>
       <div>
-        <img src="../../static/404.jpg" alt="../../static/404.jpg" />
+        <StaticImage src="../../static/404.jpg" alt="../../static/404.jpg" />
         <h2>404</h2>
         <p>Page existence is a lie</p>
       </div>
     </Layout>
   )
 }
+
+export default NotFound
