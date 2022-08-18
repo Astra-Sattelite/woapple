be3ccda8a37720c6d3b41454a7807e9e3533f44c
ydrExty <liaroffear@mail.ru>
2022-08-18 16:03:45 +0500

    Updated Featured visual

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -27,13 +27,31 @@ const FeaturedButtons = () => {
   return (
     <div className="flex flex-row gap-2 text-white">
       <div 
-        className="p-5 w-0 h-0 border-2 rounded-full flex justify-center items-center text-1xl select-none"
+        className="
+          p-5 w-0 
+          h-0 border-2 
+          rounded-full flex 
+          justify-center items-center 
+          text-1xl select-none
+          hover:bg-white
+          hover:text-black
+          transition ease-in-out duration-500
+        "
         onClick={() => scrollR(-scrollBy, "scrollableFeatured")}
       >
         {"<"}
       </div>
       <div 
-        className="p-5 w-0 h-0 border-2 rounded-full flex justify-center items-center text-1xl select-none"
+        className="
+          p-5 w-0 
+          h-0 border-2 
+          rounded-full flex 
+          justify-center items-center 
+          text-1xl select-none
+          hover:bg-white
+          hover:text-black
+          transition ease-in-out duration-500
+        "
         onClick={() => scrollR(scrollBy, "scrollableFeatured")}
       >
         {">"}
@@ -50,10 +68,18 @@ const FeaturedPost = (props: DatoCmsPost) => {
   return (
     <Link 
       to={"/post/" + props.slug} id="featuredPost1"
-      className="inline-block border-2 h-5/6 w-11/12 m-5 relative"
+      className="inline-block h-5/6 w-8/12 m-5 p-25 relative"
     >
-      <GatsbyImage image={image} alt="" className="w-full h-full" />
-      <div className="absolute bottom-5 left-5 m-auto text-white text-5xl w-11/12 h-24 text-left overflow-hidden flex flex-col">
+      <GatsbyImage image={image} alt="" className="w-full h-full rounded-2xl" />
+      <div className="
+          absolute bottom-5 
+          left-5 m-auto 
+          text-white text-5xl 
+          w-11/12 h-24 text-left 
+          overflow-hidden flex 
+          flex-col
+        "
+      >
         <p>{props.title}</p>
         <p className="text-3xl">{date}</p>
       </div>
@@ -86,26 +112,34 @@ const Featured = () => {
 
   return (
     <div className="w-screen h-screen bg-black flex flex-col justify-center items-center">
-      <div className="border-white w-11/12 h-8 flex flex-row justify-between text-white">
-        <div className="font-bold text-left text-4xl">
+      <div className="border-white w-9/12 h-8 flex flex-row justify-between text-white">
+        <div className="font-bold text-left text-3xl">
           Featured Stories
         </div>
         <Link 
           to="/stories" 
-          className="rounded-3xl text-2xl p-5 border-2 border-white flex items-center select-none"
+          className="
+            rounded-3xl text-1xl 
+            p-5 border-2 
+            border-white flex 
+            items-center select-none
+            hover:bg-white
+            hover:text-black
+            transition ease-in-out duration-500
+          "
         >
           View All
         </Link>
       </div>
       <div 
-        className="h-3/4 pb-4 pt-4 overflow-x-scroll whitespace-nowrap overflow-hidden no-scrollbar scroll-smooth"
+        className="h-5/6 pb-4 w-full pt-4 overflow-x-scroll whitespace-nowrap overflow-hidden no-scrollbar scroll-smooth"
         id="scrollableFeatured"
       >
         {data.datoCmsFeatured.featuredposts.map(
           post => <FeaturedPost {...post} key={"__featuredpostk" + post.slug} />
         )}
       </div>
-      <div className="border-white w-11/12 h-8 flex flex-row justify-between">
+      <div className="border-white w-9/12 h-8 flex flex-row justify-between">
         <FeaturedCircles num={data.datoCmsFeatured.featuredposts.length} />
         <FeaturedButtons/>
       </div>
